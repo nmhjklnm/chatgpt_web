84fe03327c795bb754fc5c83a9c1b5d9622f2be4
yyh <1973755432@qq.com>
2023-03-06 16:58:15 +0800

    自key版

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ st.set_page_config(page_title="Your App", layout="wide")
 
 import streamlit as st
 
-keys = ["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",
-            "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",
-            "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",
-            "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",
-            "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",
-            "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"
-            ]
 
 models = {
         "gpt-3.5-turbo": "gpt-3.5-turbo",
@@ -65,12 +58,23 @@ def main():
         if "state" not in st.session_state:
             st.session_state.state = get_state()
 
-        col1, col2,col3 = st.container(),st.container(),st.container()
+        col1, col2,col3= st.container(),st.container(),st.container()
         st.title("")
+
+
         with col2:
             st.header("对话")
-            model = st.selectbox("选择模型", list(models.keys()))
+            keys=[]
             message = st.text_area("You", height=100, value="", key="input")
+            with st.expander("填充你自己的API"):
+                st.write("输入你的openai-key,并回车")
+                api_key1,api_key2,api_key3 = st.text_input("api-key1"),st.text_input("api-key2"),st.text_input("api-key3")
+                keys = [key for key in [api_key1, api_key2, api_key3] if key] # 如果存在就加进去
+                st.write(keys)
+
+
+            model = st.selectbox("选择模型", list(models.keys()))
+
             if st.button("发送",use_container_width=True):
                 chat_history = st.session_state.state["chat_history"]
                 # 清除重复聊天记录
@@ -101,6 +105,7 @@ def main():
             for msg in st.session_state.state["chat_history"]:
                 st.markdown("---")
                 st.write(msg)
+
         with col3:
             chat_count = sum(1 for item in st.session_state.state["chat_history"] if isinstance(item, str)
                          or (isinstance(item, dict)
@@ -114,9 +119,10 @@ def main():
 
     elif choice == "开发历史":
         st.title("2023年3月3：开始开发 ChatGPT Web")
-        st.write("[2022年3月4：ChatGPT web v1.0 版本发布](https://gitee.com/yang1he/chatgpt-web-app)")
-        st.write("[2022年3月5：ChatGPT web v2.0 版本发布](https://gitee.com/yang1he/chatgpt-web2-app)")
-        st.write("[2022年3月6：ChatGPT web v3.0 版本发布](https://gitee.com/yang1he/chatgpt-web3-app)")
+        st.write("[2022年3月4：ChatGPT web v0.1.0 版本发布](https://gitee.com/yang1he/chatgpt-web-app)")
+        st.write("[2022年3月5：ChatGPT web v0.2 版本发布](https://gitee.com/yang1he/chatgpt-web2-app)")
+        st.write("[2022年3月6：ChatGPT web v0.3.0 版本发布](https://gitee.com/yang1he/chatgpt-web3-app)")
+        st.write("[2022年3月6：ChatGPT web 需多key版](https://gitee.com/yang1he/chatgpt-web3-app)")
         st.empty()
 
 
