2a33c94933d81988fef96181c954fd3c41189044
yyh <1973755432@qq.com>
2023-03-10 11:01:19 +0800

    最新创新版本

---
+++ test.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+import numpy as np
+
+audio_file = open('myaudio.ogg', 'rb')
+audio_bytes = audio_file.read()
+
+st.audio(audio_bytes, format='audio/ogg')
+
+sample_rate = 44100  # 44100 samples per second
+seconds = 2  # Note duration of 2 seconds
+frequency_la = 440  # Our played note will be 440 Hz
+# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+t = np.linspace(0, seconds, seconds * sample_rate, False)
+# Generate a 440 Hz sine wave
+note_la = np.sin(frequency_la * t * 2 * np.pi)
+
+st.audio(note_la, sample_rate=sample_rate)
