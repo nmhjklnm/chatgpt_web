c8c0842695a28499800aacd0e15e5a79010832aa
Yang YiHe <108562510+nmhjklnm@users.noreply.github.com>
2023-05-23 17:17:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import streamlit as st
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.write("[2022年3月4：ChatGPT web v0.1.0 版本发布](https://gitee.com/yang1he/chatgpt-web-app)")
 st.write("[2022年3月5：ChatGPT web v0.2 版本发布](https://gitee.com/yang1he/chatgpt-web2-app)")
 st.write("[2022年3月6：ChatGPT web v0.3.0 版本发布](https://gitee.com/yang1he/chatgpt-web3-app)")
